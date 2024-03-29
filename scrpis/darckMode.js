7b4b88ed1ed6afc905177731e5f1f3f8bba41f2c
Santiago Collaud <santicoll_00@yahoo.com.ar>
2024-03-29 16:30:42 -0300

    modo oscuro funciona index

--- scrpis/darckMode.js
+++ scrpis/darckMode.js
@@ -7,8 +7,19 @@ function cambiarDarkMode() {
     if (stylesheet.href.endsWith(Stylesheet)) {
         // Cambia a la hoja de estilo oscuro
         stylesheet.href = darkStylesheet;
+        localStorage.setItem('modo', 'oscuro'); // Guarda el estado del modo oscuro en el almacenamiento local
     } else {
         // Cambia a la hoja de estilo clara
         stylesheet.href = Stylesheet;
+        localStorage.setItem('modo', 'normal');// Guarda el estado del modo normal en el almacenamiento local
     }
 }
+cambiarDarkMode();
+// Verificar el estado del modo al cargar la página
+window.onload = function() {
+    var modoAlmacenado = localStorage.getItem('modo');
+    if (modoAlmacenado === 'oscuro') {
+        // Aplicar el modo oscuro si está almacenado
+        cambiarDarkMode();
+    }
+};
